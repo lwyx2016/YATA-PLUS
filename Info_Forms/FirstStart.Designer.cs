aa0a93087e0c7274d09daef5d225a1843a90f486
exelix11 <ex.elix@libero.it>
2015-09-13 17:38:21 +0200

    last fixes + updated readme

--- Info_Forms/FirstStart.Designer.cs
+++ Info_Forms/FirstStart.Designer.cs
@@ -50,7 +50,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(304, 244);
+            this.button1.Location = new System.Drawing.Point(304, 263);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(134, 30);
             this.button1.TabIndex = 1;
@@ -69,7 +69,7 @@
             // label3
             // 
             this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.label3.Location = new System.Drawing.Point(2, 262);
+            this.label3.Location = new System.Drawing.Point(2, 272);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(153, 30);
             this.label3.TabIndex = 3;
@@ -90,7 +90,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(49, 218);
+            this.linkLabel1.Location = new System.Drawing.Point(51, 232);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(359, 13);
             this.linkLabel1.TabIndex = 4;
@@ -102,7 +102,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(445, 280);
+            this.ClientSize = new System.Drawing.Size(445, 296);
             this.ControlBox = false;
             this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.linkLabel1);
